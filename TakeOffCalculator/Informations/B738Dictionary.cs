569622ad1551324a90d919431b0ccd343ea4bca6
Tiago Vicente <tmavicente@gmail.com>
2018-02-05 02:01:30 +0000

    1ºst of day

--- TakeOffCalculator/Informations/B738Dictionary.cs
+++ TakeOffCalculator/Informations/B738Dictionary.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 
 
 public class B738Dictionary
 {
-	public static B738Dictionary()
-	{
-        new Dictionary<short,  PressAltTemperature>()
-        {
-            
-        }
-	}
+
 }
--- TakeOffCalculator/Informations/DictionaryDeclaration.cs
+++ TakeOffCalculator/Informations/DictionaryDeclaration.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Device.Location;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 public class DictionaryDeclaration
 {
