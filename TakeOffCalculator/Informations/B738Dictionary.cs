5bd381c285f6d7a301963624c51912f6d13b0cbd
Tiago Vicente <tmavicente@gmail.com>
2018-02-05 01:52:59 +0000

    firstcommit

---
+++ TakeOffCalculator/Informations/B738Dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+public class B738Dictionary
+{
+	public static B738Dictionary()
+	{
+        new Dictionary<short,  PressAltTemperature>()
+        {
+            
+        }
+	}
+}
---
+++ TakeOffCalculator/Informations/DictionaryDeclaration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+
+public class DictionaryDeclaration
+{
+    public class AircraftPerformance
+    {
+        public Dictionary DictionaryDeclaration(Dictionary<short, PressAltTemperature> PressAltTemperature )
+        {
+            this.PressAltTemperature = PressAltTemperature;
+        }
+
+        public Dictionary<short, PressAltTemperature> PressAltTemperature
+        { get; private set; }
+    }
+   
+
+    public class PressAltTemperature
+    {
+        public PressAltTemperature(string result)
+        {
+            this.result = result;
+        }
+
+        public string result
+        { get; private set; }
+
+    }
+}
